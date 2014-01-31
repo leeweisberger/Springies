4a1f21b1d03569c9eda4c894c5b2f2f14acae5dd
Lee Weisberger <lee.weisberger@duke.edu>
2014-01-30 23:53:14 -0500

    created empty Mass class

---
+++ src/springies/Mass.java
@@ -0,0 +1,15 @@
+package springies;
+
+import jboxGlue.PhysicalObject;
+import jboxGlue.PhysicalObjectCircle;
+
+import org.jbox2d.common.Vec2;
+
+import jgame.JGColor;
+import jgame.JGObject;
+import jgame.platform.JGEngine;
+
+public class Mass {
+	
+	
+}
