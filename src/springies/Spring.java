c64fe8b8b2ea70f255279d42d57b13a871d1fbd2
Jimmy Fang <jimmy.fang@duke.edu>
2014-02-01 15:51:26 -0500

    Updated functionality on springs, overrided paint() instead of using setPos()

--- src/springies/Spring.java
+++ src/springies/Spring.java
@@ -9,27 +9,35 @@ import jgame.platform.JGEngine;
 
 public class Spring extends PhysicalObjectRect{
 	private PhysicalObject mySpring;
-	 private Mass myM1;
-	 private Mass myM2;
+	private Mass myM1;
+	private Mass myM2;
+
 	public Spring(Mass m1, Mass m2, double rlength, double springiness){
-		 
-		 super(m1.toString()+m2.toString(), 2, JGColor.green, 1, rlength);
-		 float f = (float) 0;
-		// setAngle(145);
-		 System.out.println();
-		 x=m1.x;
-		 y=m1.y;
-		 setPos((m1.x + m2.x)/2, (m1.y+m2.x)/2);
-		 //System.out.println(x);
-		 myM1=m1;
-		 myM2=m2;
+
+		super(m1.toString()+m2.toString(), 2, JGColor.green, 1, rlength);
+		float f = (float) 0;
+		//		System.out.println();
+		x=m1.x;
+		y=m1.y;
+		//		setPos((m1.x),(m2.y));
+		setPos((m1.x + m2.x)/2, (m1.y+m2.x)/2);
+		//		setForce((m1.x + m2.x)/2, (m1.y+m2.x)/2);
+		//System.out.println(x);
+		myM1=m1;
+		myM2=m2;
+		paint();
 	}
-	
+
 	public void move(){
 		//System.out.println(myM1.y);
 		//System.out.println(x);
 	}
 	
-	
-
+	@Override
+	public void paint(){
+		double[] myX = {myM1.x,myM2.x};
+		double[] myY = {myM1.y,myM2.y};
+		JGColor[] myColors = {JGColor.green,JGColor.blue};
+		myEngine.drawPolygon(myX, myY, myColors, 2, false, false);
+	}
 }
--- src/springies/Springies.java
+++ src/springies/Springies.java
@@ -19,127 +19,130 @@ public class Springies extends JGEngine
 
 {
 	private HashMap<String,Mass> m = new HashMap<String,Mass>();
-    public Springies ()
-    {
-        // set the window size
-        int height = 800;
-        double aspect = 16.0 / 9.0;
-        initEngineComponent((int) (height * aspect), height);
-    }
+	public Springies ()
+	{
+		// set the window size
+		int height = 800;
+		double aspect = 16.0 / 9.0;
+		initEngineComponent((int) (height * aspect), height);
+	}
 
-    @Override
-    public void initCanvas ()
-    {
-        // I have no idea what tiles do...
-        setCanvasSettings(1, // width of the canvas in tiles
-                          1, // height of the canvas in tiles
-                          displayWidth(), // width of one tile
-                          displayHeight(), // height of one tile
-                          null,// foreground colour -> use default colour white
-                          null,// background colour -> use default colour black
-                          null); // standard font -> use default font
-    }
+	@Override
+	public void initCanvas ()
+	{
+		// I have no idea what tiles do...
+		setCanvasSettings(1, // width of the canvas in tiles
+				1, // height of the canvas in tiles
+				displayWidth(), // width of one tile
+				displayHeight(), // height of one tile
+				null,// foreground colour -> use default colour white
+				null,// background colour -> use default colour black
+				null); // standard font -> use default font
+	}
 
-    @Override
-    public void initGame ()
-    {
-        setFrameRate(60, 2);
-        // NOTE:
-        //   world coordinates have y pointing down
-        //   game coordinates have y pointing up
-        // so gravity is up in world coords and down in game coords
-        // so set all directions (e.g., forces, velocities) in world coords
-        WorldManager.initWorld(this);
-        WorldManager.getWorld().setGravity(new Vec2(0.0f, 0.1f));
-       
-        addMasses();
-       addSprings();
-        addWalls();
-        
-    }
-    
-    public void addMasses ()
-    {
-    	XML_Parser p = new XML_Parser();
-    	p.parse();
-    	HashMap<String,Integer[]> masseslist = new HashMap<String,Integer[]>();
-    	masseslist=p.masses;
-    	for(String mass:masseslist.keySet()){    		
-    		m.put(mass, new Mass(mass,masseslist.get(mass)[0],masseslist.get(mass)[1]));	
-    	}
-        // add a bouncy ball
-        // NOTE: you could make this into a separate class, but I'm lazy
-    	
-//        PhysicalObject ball = new PhysicalObjectCircle("ball", 1, JGColor.blue, 10, 5) {
-//            @Override
-//            public void hit (JGObject other)
-//            {
-//                // we hit something! bounce off it!
-//                Vec2 velocity = myBody.getLinearVelocity();
-//                // is it a tall wall?
-//                final double DAMPING_FACTOR = 0.8;
-//                boolean isSide = other.getBBox().height > other.getBBox().width;
-//                if (isSide) {
-//                    velocity.x *= -DAMPING_FACTOR;
-//                }
-//                else {
-//                    velocity.y *= -DAMPING_FACTOR;
-//                }
-//                // apply the change
-//                myBody.setLinearVelocity(velocity);
-//            }
-//        };
-//        ball.setPos(displayWidth() / 2, displayHeight() / 2);
-//        ball.setForce(8000, -10000);
-    }
-    public void addSprings(){
-    	XML_Parser p = new XML_Parser();
-    	p.parse();
-    	ArrayList<String[]> springslist=p.springs;  
-    	
-    	for(String[] spring:springslist){  
-    		//System.out.println(spring[0]);
-    		new Spring(m.get(spring[0]), m.get(spring[1]), Double.parseDouble(spring[3]), Double.parseDouble(spring[2]));
-    		//System.out.println("name: " + m.get(spring[0]).x + "end");
-    	}
-    	
-    }
+	@Override
+	public void initGame ()
+	{
+		setFrameRate(60, 2);
+		// NOTE:
+		//   world coordinates have y pointing down
+		//   game coordinates have y pointing up
+		// so gravity is up in world coords and down in game coords
+		// so set all directions (e.g., forces, velocities) in world coords
+		WorldManager.initWorld(this);
+		WorldManager.getWorld().setGravity(new Vec2(0.0f, 0.1f));
 
-    private void addWalls ()
-    {
-        // add walls to bounce off of
-        // NOTE: immovable objects must have no mass
-        final double WALL_MARGIN = 10;
-        final double WALL_THICKNESS = 10;
-        final double WALL_WIDTH = displayWidth() - WALL_MARGIN * 2 + WALL_THICKNESS;
-        final double WALL_HEIGHT = displayHeight() - WALL_MARGIN * 2 + WALL_THICKNESS;
-        PhysicalObject wall = new PhysicalObjectRect("wall", 2, JGColor.green,
-                                                     WALL_WIDTH, WALL_THICKNESS);
-        wall.setPos(displayWidth() / 2, WALL_MARGIN);
-        wall = new PhysicalObjectRect("wall", 2, JGColor.green,
-                                      WALL_WIDTH, WALL_THICKNESS);
-        wall.setPos(displayWidth() / 2, displayHeight() - WALL_MARGIN);
-        wall = new PhysicalObjectRect("wall", 2, JGColor.green,
-                                      WALL_THICKNESS, WALL_HEIGHT);
-        wall.setPos(WALL_MARGIN, displayHeight() / 2);
-        wall = new PhysicalObjectRect("wall", 2, JGColor.green,
-                                      WALL_THICKNESS, WALL_HEIGHT);
-        wall.setPos(displayWidth() - WALL_MARGIN, displayHeight() / 2);
-    }
+		addMasses();
+		addSprings();
+		addWalls();
 
-    @Override
-    public void doFrame ()
-    {
-        // update game objects
-        WorldManager.getWorld().step(1f, 1);
-        moveObjects();
-        //checkCollision(1 + 2, 1);
-    }
+	}
 
-    @Override
-    public void paintFrame ()
-    {
-        // nothing to do
-        // the objects paint themselves
-    }
+	public void addMasses ()
+	{
+		XML_Parser p = new XML_Parser();
+		p.parse();
+		HashMap<String,Integer[]> masseslist = new HashMap<String,Integer[]>();
+		masseslist=p.masses;
+		for(String mass:masseslist.keySet()){    		
+			m.put(mass, new Mass(mass,masseslist.get(mass)[0],masseslist.get(mass)[1]));	
+		}
+		// add a bouncy ball
+		// NOTE: you could make this into a separate class, but I'm lazy
+
+		//        PhysicalObject ball = new PhysicalObjectCircle("ball", 1, JGColor.blue, 10, 5) {
+		//            @Override
+		//            public void hit (JGObject other)
+		//            {
+		//                // we hit something! bounce off it!
+		//                Vec2 velocity = myBody.getLinearVelocity();
+		//                // is it a tall wall?
+		//                final double DAMPING_FACTOR = 0.8;
+		//                boolean isSide = other.getBBox().height > other.getBBox().width;
+		//                if (isSide) {
+		//                    velocity.x *= -DAMPING_FACTOR;
+		//                }
+		//                else {
+		//                    velocity.y *= -DAMPING_FACTOR;
+		//                }
+		//                // apply the change
+		//                myBody.setLinearVelocity(velocity);
+		//            }
+		//        };
+		//        ball.setPos(displayWidth() / 2, displayHeight() / 2);
+		//        ball.setForce(8000, -10000);
+	}
+	public void addSprings(){
+		XML_Parser p = new XML_Parser();
+		p.parse();
+		ArrayList<String[]> springslist=p.springs;  
+		for(String[] spring:springslist){  
+			//System.out.println(spring[0]);
+			Mass m1 = m.get(spring[0]);
+			Mass m2 = m.get(spring[1]);
+			new Spring(m.get(spring[0]), m.get(spring[1]), Double.parseDouble(spring[3]), Double.parseDouble(spring[2]));
+			
+//			s.setPos((m1.getLastX() + m2.getLastY())/2, (m1.getLastY() + m2.getLastY())/2);
+			//System.out.println("name: " + m.get(spring[0]).x + "end");
+		}
+
+	}
+
+	private void addWalls ()
+	{
+		// add walls to bounce off of
+		// NOTE: immovable objects must have no mass
+		final double WALL_MARGIN = 10;
+		final double WALL_THICKNESS = 10;
+		final double WALL_WIDTH = displayWidth() - WALL_MARGIN * 2 + WALL_THICKNESS;
+		final double WALL_HEIGHT = displayHeight() - WALL_MARGIN * 2 + WALL_THICKNESS;
+		PhysicalObject wall = new PhysicalObjectRect("wall", 2, JGColor.green,
+				WALL_WIDTH, WALL_THICKNESS);
+		wall.setPos(displayWidth() / 2, WALL_MARGIN);
+		wall = new PhysicalObjectRect("wall", 2, JGColor.green,
+				WALL_WIDTH, WALL_THICKNESS);
+		wall.setPos(displayWidth() / 2, displayHeight() - WALL_MARGIN);
+		wall = new PhysicalObjectRect("wall", 2, JGColor.green,
+				WALL_THICKNESS, WALL_HEIGHT);
+		wall.setPos(WALL_MARGIN, displayHeight() / 2);
+		wall = new PhysicalObjectRect("wall", 2, JGColor.green,
+				WALL_THICKNESS, WALL_HEIGHT);
+		wall.setPos(displayWidth() - WALL_MARGIN, displayHeight() / 2);
+	}
+
+	@Override
+	public void doFrame ()
+	{
+		// update game objects
+		WorldManager.getWorld().step(1f, 1);
+		moveObjects();
+		//checkCollision(1 + 2, 1);
+	}
+
+	@Override
+	public void paintFrame ()
+	{
+		// nothing to do
+		// the objects paint themselves
+	}
 }
